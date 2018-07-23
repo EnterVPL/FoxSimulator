c59260b0c477f9efc297626822bb3c0a49803daf
Łukasz <woramowy@gmail.com>
2018-07-23 22:04:37 +0200

    Add random color and fast setLanguage.

--- classesJS/Controler.js
+++ classesJS/Controler.js
@@ -7,6 +7,7 @@
 function Controller() {
 
     var self = this;
+    var trans;
 
     /**
      * Created Translate object and translation.
@@ -14,16 +15,47 @@ function Controller() {
      * @param  {array} langList - List of support languages
      */
     this.translate = function (lang, langList) {
-        var trans = new Translate(lang, langList);
+        trans = new Translate(lang, langList);
         trans.getCountry();
         trans.setLang();
     }
+
+    /**
+     * !!! After use translate !!! 
+     * @param  {string} lang
+     */
+    this.setLanguage = function (lang) {
+        trans.setLang(lang);
+    }
+
+
     /**
      * @param  {string} element - Set text cursor on "element". Use jQuery.(element)
      */
     this.setDefaultFocus = function (element) {
         var val = $(element).text();
         $(element).focus();
+    }
+
+    /**
+     * @returns {string} HEX color
+     */
+    this.getRandomColor = function () {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    /**
+     * Set random color for <element style="css">
+     * @param  {string} element
+     * @param  {string} css
+     */
+    this.setRandomColor = function (element, css) {
+        $(element).css(css, self.getRandomColor());
     }
 
 }
--- classesJS/buttonsEvents.js
+++ classesJS/buttonsEvents.js
@@ -1,15 +1,18 @@
 $('document').ready(function () {
 
     $('#langs').on("click", 'div', function () {
-        var trans = new Translate();
         var newLang = $(this).attr('class');
         console.log(newLang);
-        trans.setLang(newLang);
+        controller.setLanguage(newLang);
     });
 
     $('#consoleCommands').on("click", "p", function () {
         console.log($(this).attr("command"));
         $("#consoleInput").text($(this).attr("command"));
+
+        controller.setRandomColor("div#consoleOutput, div#consoleInput", "border-color");
+        controller.setRandomColor("div#consoleCommands, div#consoleCommands *", "border-color");
+        controller.setRandomColor("div#langs *", "border-color");
     });
 
 });
